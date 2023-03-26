140a2fc2ad17982d0f563d0d9e3bda098821aca1
Sironi-00 <00sironi@gmail.com>
2023-03-26 16:42:15 +0100

    ++ to E, -- HashTag

--- script.js
+++ script.js
@@ -7,10 +7,11 @@
 let string = document.getElementById("input");
 
 let Alphabets = {
-    'A':"Ace", 'B':"Banana", 'C':"Chocolate", 'D':"Delete", 'E':"Elite", 'F':"FOSS", 'G':"Gain", 
-    'H':"Hope", 'I':"Intuition", 'J':"Jade", 'K':"Kage", 'L':"Limbo",'M':"Moon", 'N':"Noted", 
-    'O':"Obligation", 'P':"Pleasure", 'Q':"Quick", 'R':"Resolve", 'S':"Rigid", 'T':"Tatsu", 
-    'U':"Umbra", 'V':"Vauban", 'W':"Water", 'X':"Xitsonga",'Y':"Yew", 'Z':"Zen", " ":"|_|", ".":"\<\>",
+    'A':`<div class="alphabet A"></div>`, 'B':`<div class="alphabet B"></div>`, 'C':`<div class="alphabet C"></div>`, 'D':`<div class="alphabet D"></div>`,
+     'E':`<div class="alphabet E"></div>`, 'F':`<div class="alphabet F"></div>`, 'G':`<div class="alphabet G"></div>`, 'H':`<div class="alphabet H"></div>`, 
+     'I':`<div class="alphabet I"></div>`, 'J':`<div class="alphabet J"></div>`, 'K':`<div class="alphabet K"></div>`, 'L':`<div class="alphabet D"></div>`, 
+     'M':"Moon", 'N':"Noted", 'O':"Obligation", 'P':"Pleasure", 'Q':"Quick", 'R':"Resolve", 'S':"Rigid", 'T':"Tatsu",  
+     'U':"Umbra", 'V':"Vauban", 'W':"Water", 'X':"Xitsonga",'Y':"Yew", 'Z':"Zen", " ":"|_|", ".":"\<\>",
     '0':`<div class=\"zero\">This would be a div</div>`, '#':`<div class="alphabet hashTag"></div>`
 }
 
