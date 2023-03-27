da59069b05d4ced6d04d30cbcd6dd0a21d0c5c9c
Sironi-00 <00sironi@gmail.com>
2023-03-27 23:53:37 +0100

    ++ to L, animate 0.01 ++

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ let Alphabets = {
     'A':`<div class="alphabet A"></div>`, 'B':`<div class="alphabet B"></div>`, 'C':`<div class="alphabet C"></div>`, 'D':`<div class="alphabet D"></div>`,
      'E':`<div class="alphabet E"></div>`, 'F':`<div class="alphabet F"></div>`, 'G':`<div class="alphabet G"></div>`, 'H':`<div class="alphabet H"></div>`, 
      'I':`<div class="alphabet I"></div>`, 'J':`<div class="alphabet J"></div>`, 'K':`<div class="alphabet K"></div>`, 'L':`<div class="alphabet L"></div>`, 
-     'M':"Moon", 'N':"Noted", 'O':"Obligation", 'P':"Pleasure", 'Q':"Quick", 'R':"Resolve", 'S':"Rigid", 'T':"Tatsu",  
-     'U':"Umbra", 'V':"Vauban", 'W':"Water", 'X':"Xitsonga",'Y':"Yew", 'Z':"Zen", " ":"|_|", ".":"\<\>",
-    '0':`<div class=\"zero\">This would be a div</div>`, '#':`<div class="alphabet hashTag"></div>`
+     'M':`<div class="alphabet M"></div>`, 'N':`<div class="alphabet N"></div>`, 'O':`<div class="alphabet O"></div>`, 'P':`<div class="alphabet P"></div>`, 
+     'Q':`<div class="alphabet Q"></div>`, 'R':`<div class="alphabet R"></div>`, 'S':`<div class="alphabet S"></div>`, 'T':`<div class="alphabet S"></div>`,  
+     'U':`<div class="alphabet U"></div>`, 'V':`<div class="alphabet V"></div>`, 'W':`<div class="alphabet W"></div>`, 'X':`<div class="alphabet X"></div>`,  
+     'Y':`<div class="alphabet Y"></div>`, 'Z':`<div class="alphabet Z"></div>`, '#':`<div class="alphabet hashTag"></div>`
 }
 
 let makeString = (letter) => {
